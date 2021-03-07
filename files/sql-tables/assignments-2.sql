844ea48d2319890db9ea15c9418744861979f8c1
Justin Skycak <jpskycak@gmail.com>
2021-03-06 19:08:37 -0500

    Update assignments-2.sql

--- files/sql-tables/assignments-2.sql
+++ files/sql-tables/assignments-2.sql
@@ -1,54 +1,36 @@
-CREATE TABLE age ( 
-    lastname VARCHAR(30), 
-    age TINYINT
+CREATE TABLE student ( 
+    id INT,
+    lastname VARCHAR(50), 
+    firstname VARCHAR(50)
 ); 
  
-INSERT INTO `age` (`lastname`, `age`)
+INSERT INTO `student` (`id`, `firstname`, `lastname`)
 VALUES
-    ('Walton', 15),
-    ('Sanchez', 15),
-    ('Ng', 16),
-    ('Smith', 16),
-    ('Shenko', 17); 
- 
-CREATE TABLE name ( 
-    lastname VARCHAR(30), 
-    firstname VARCHAR(30)
-); 
- 
-INSERT INTO `name` (`firstname`, `lastname`)
-VALUES
-    ('Franklin', 'Walton'),
-    ('Sylvia', 'Sanchez'),
-    ('Harry', 'Ng'),
-    ('Ishmael', 'Smith'),
-    ('Kinga', 'Shenko'); 
+    (1, 'Franklin', 'Walton'),
+    (2, 'Sylvia', 'Sanchez'),
+    (3, 'Harry', 'Ng'),
+    (4, 'Ishmael', 'Smith'),
+    (5, 'Kinga', 'Shenko'); 
 
 CREATE TABLE assignment ( 
-    number TINYINT,
-    lastname VARCHAR(30), 
-    score TINYINT
+    id INT,
+    studentId INT, 
+    score INT
 ); 
  
-INSERT INTO `assignment` (`number`, `lastname`, `score`)
+INSERT INTO `assignment` (`id`, `studentId`, `score`)
 VALUES
-    (1, 'Walton', 90),
-    (1, 'Sanchez', 100),
-    (1, 'Ng', 95),
-    (1, 'Smith', 80),
-    #(1, 'Shenko', 85),
-    (2, 'Walton', 90),
-    (2, 'Sanchez', 90),
-    (2, 'Ng', 75),
-    (2, 'Smith', 100),
-    (2, 'Shenko', 95),
-    (3, 'Walton', 60),
-    #(3, 'Sanchez', 70),
-    (3, 'Ng', 65),
-    (3, 'Smith', 90),
-    (3, 'Shenko', 75);
-    (4, 'Walton', 90),
-    #(4, 'Sanchez', 100),
-    (4, 'Ng', 55),
-    (4, 'Smith', 95),
-    (4, 'Shenko', 80);
+    (101, 2, 100),
+    (101, 3, 95),
+    (101, 4, 80),
+    (102, 1, 90),
+    (102, 2, 90),
+    (102, 3, 75),
+    (102, 4, 100),
+    (102, 5, 95),
+    (103, 3, 65),
+    (103, 4, 90),
+    (103, 5, 75),
+    (104, 3, 55),
+    (104, 4, 95),
+    (104, 5, 80);
