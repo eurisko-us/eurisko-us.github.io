e0c14143d66a40745e624e4e2791860f623937db
Justin Skycak <jpskycak@gmail.com>
2022-02-01 14:06:18 -0500

    Create 1.sql

---
+++ files/sql-tables/1.sql
@@ -0,0 +1,20 @@
+CREATE TABLE people ( 
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(30) NOT NULL, 
+age VARCHAR(50)
+); 
+
+INSERT INTO `people` (`id`, `name`, `age`)
+VALUES ('1', 'Franklin', '12'); 
+
+INSERT INTO `people` (`id`, `name`, `age`)
+VALUES ('2', 'Sylvia', '13'); 
+
+INSERT INTO `people` (`id`, `name`, `age`)
+VALUES ('3', 'Harry', '14'); 
+
+INSERT INTO `people` (`id`, `name`, `age`)
+VALUES ('4', 'Ishmael', '15'); 
+
+INSERT INTO `people` (`id`, `name`, `age`)
+VALUES ('5', 'Kinga', '16');
